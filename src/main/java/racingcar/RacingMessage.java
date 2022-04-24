3255d5205d9d430d04d71893b8a9287dec1f2977
jordan <jordan@hpcnt.com>
2022-04-25 00:43:10 +0900

    feature<Lap>: Lap에 대한 기능 구현

--- src/main/java/racingcar/RacingMessage.java
+++ src/main/java/racingcar/RacingMessage.java
@@ -7,6 +7,8 @@ public class RacingMessage {
     public static final String MESSAGE_ERROR_EMPTY = MESSAGE_ERROR + "자동차 이름을 입력하여 주세요.";
     public static final String MESSAGE_ERROR_OVER_LENGTH = MESSAGE_ERROR + "자동차 이름은 " + COMPARE_LENGTH + "5글자를 넘을 수 없습니다.";
     public static final String MESSAGE_ERROR_NUMBER_FORMAT = MESSAGE_ERROR + "시도 횟수는 숫자여야 합니다.";
-
+    public static final String MESSAGE_SPACE = " ";
+    public static final String MESSAGE_COLON = ":";
+    public static final String MESSAGE_LAP = "-";
 
 }
